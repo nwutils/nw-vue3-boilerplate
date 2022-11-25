fb5bb1ea3320a69325c83855e0d9f9dd4a7170a4
TheJaredWilcurt <TheJaredWilcurt@users.noreply.github.com>
2022-11-25 12:00:53 -0500

    PiniaDemo tests

---
+++ tests/unit/components/PiniaDemo.test.js
@@ -0,0 +1,55 @@
+import testHelpers from '@@/unit/test-helpers.js';
+
+import PiniaDemo from '@/components/PiniaDemo.vue';
+
+describe('PiniaDemo.vue', () => {
+  const setupWrapper = async () => {
+    const wrapper = await testHelpers.mount(PiniaDemo);
+    wrapper.vm.counterStore.resetState();
+    return wrapper;
+  };
+
+  describe('Desktop', () => {
+    test('Render default contents', async () => {
+      const wrapper = await setupWrapper();
+
+      expect(wrapper)
+        .toMatchSnapshot();
+    });
+
+    test('Click increment', async () => {
+      const wrapper = await setupWrapper();
+
+      const button = wrapper.find('[data-test="incrementButton"]');
+      await button.trigger('click');
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper)
+        .toMatchSnapshot();
+    });
+  });
+
+  describe('Web', () => {
+    beforeEach(() => {
+      window.webSetup();
+    });
+
+    test('Render default contents', async () => {
+      const wrapper = await setupWrapper();
+
+      expect(wrapper)
+        .toMatchSnapshot();
+    });
+
+    test('Click increment', async () => {
+      const wrapper = await setupWrapper();
+
+      const button = wrapper.find('[data-test="incrementButton"]');
+      await button.trigger('click');
+      await wrapper.vm.$nextTick();
+
+      expect(wrapper)
+        .toMatchSnapshot();
+    });
+  });
+});
--- tests/unit/test-helpers.js
+++ tests/unit/test-helpers.js
@@ -3,11 +3,13 @@ import { shallowMount, mount } from '@vue/test-utils';
 
 import applyPrototypes from '@/helpers/applyPrototypes.js';
 
+const pinia = createTestingPinia({ stubActions: false });
+
 export default {
   setupOptions: function (options) {
     options = options || {};
     options.global = options.global || {};
-    options.global.plugins = [createTestingPinia()];
+    options.global.plugins = [pinia];
     options.global.config = options.global.config || {};
     options.global.config.globalProperties = options.global.config.globalProperties || {};
     applyPrototypes(options.global.config.globalProperties);
