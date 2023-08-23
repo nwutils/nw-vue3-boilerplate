286e14f84d99e1df7c47d6d3a7bc0cbb142529e8
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2023-08-22 22:37:57 -0400

    Tweaks

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: 'happy-dom',
     coverage: {
       reportsDirectory: './tests/unit/coverage'
     },
