b0697ef067c9de58dc6516e44143879afa744237
TheJaredWilcurt <TheJaredWilcurt@user.noreply.github.com>
2023-08-24 18:54:23 -0400

    Set up new plugins in unit tests

--- tests/unit/test-helpers.js
+++ tests/unit/test-helpers.js
@@ -1,5 +1,8 @@
 import { createTestingPinia } from '@pinia/testing';
-import { shallowMount, mount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
+import constantsPlugin from 'vue-options-api-constants-plugin';
+
+import { router } from '@/router/index.js';
 
 import applyPrototypes from '@/helpers/applyPrototypes.js';
 
@@ -9,10 +12,17 @@ export default {
   setupOptions: function (options) {
     options = options || {};
     options.global = options.global || {};
-    options.global.plugins = [pinia];
+
     options.global.config = options.global.config || {};
     options.global.config.globalProperties = options.global.config.globalProperties || {};
     applyPrototypes(options.global.config.globalProperties);
+
+    options.global.plugins = [
+      constantsPlugin,
+      pinia,
+      router
+    ];
+
     return options;
   },
   shallowMount: function (component, options) {
